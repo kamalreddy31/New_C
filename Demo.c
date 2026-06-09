5b5ec4b2914f0e4d551692a7998efdb110bafe83
kamalreddy31 <kamalnathreddy546@example.com>
2026-06-09 14:41:13 +0530

    Made changes from DeveloperB

--- Demo.c
+++ Demo.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-
+int Mul(int a ,int b)
+{
+    return a * b;
+}
 int main()
 {
+    printf("Mul answer is %d ",Mul(10,20));
     printf("Hello From main Branch");
     printf("Hello From Feature-2 Branch");
     return 0;
 }
-
-// Im Developer 1
