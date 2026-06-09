db1f0d6dbee96f4d392dbcb1ce2c909eaf95f648
kamalreddy31 <kamalnathreddy546@example.com>
2026-06-09 14:55:38 +0530

    Fixed double main

--- Demo.c
+++ Demo.c
@@ -3,18 +3,19 @@ int Mul(int a ,int b)
 {
     return a * b;
 }
-int main()
-{
-    printf("Mul answer is %d ",Mul(10,20));
+
+   
 int subtract(int a, int b)
 {
     return a - b;
+}
 int add (int a,int b)
 {
     return a+b;
 }
 int main()
 {
+     printf("Mul answer is %d ",Mul(10,20));
     printf("%d",sub(10,20));
     printf("%d",add(10,20));
     printf("Hello From main Branch");
