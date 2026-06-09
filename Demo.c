53b50a56d20543db08e5bead2e8129588987a294
kamalreddy31 <119722329+kamalreddy31@users.noreply.github.com>
2026-06-09 14:57:49 +0530

    Fix function call and add missing function definition

--- Demo.c
+++ Demo.c
@@ -9,13 +9,17 @@ int subtract(int a, int b)
 {
     return a - b;
 }
+
+
 int add (int a,int b)
 {
     return a+b;
 }
+
+
 int main()
 {
-     printf("Mul answer is %d ",Mul(10,20));
+    printf("Mul answer is %d ",Mul(10,20));
     printf("%d",sub(10,20));
     printf("%d",add(10,20));
     printf("Hello From main Branch");
